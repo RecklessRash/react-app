8640943b9adb21ac05f8c376f464161c6a278702
rashminder2002 <80933649+RecklessRash@users.noreply.github.com>
2024-07-15 20:02:22 -0400

    Removed express duplicate

--- server/index.js
+++ server/index.js
@@ -1,27 +1,23 @@
 const express = require("express");
+const path = require("path");
+
 const PORT = process.env.PORT || 3001;
+
 const app = express();
-const path = require('path');
-const express = require('express');
 
-
-app.get("/api", (req, res) => {
-    res.json({ message: "Hello from server!" });
-});
-
-app.listen(PORT, () => {
-    console.log(`Server listening on ${PORT}`);
-});
-
-// Have Node serve the files for our built React app
+// Serve the static files from the React app
 app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 // Handle GET requests to /api route
 app.get("/api", (req, res) => {
-  res.json({ message: "Hello from server!" });
+  res.json({ message: "Hello from Rashminder's Server!" });
 });
 
 // All other GET requests not handled before will return our React app
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
 });
